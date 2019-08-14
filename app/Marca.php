89ca41588128034d64033e44adcc3a59aec27613
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2019-08-14 13:25:13 -0300

    Model de marca

---
+++ app/Marca.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    protected $fillable = [
+        'codigo_identificacao',
+        'nome',
+        'cpf',
+        'telefone',
+        'email',
+        'endereco',
+        'data_registro',
+    ];
+
+    public function setCpfAttribute($cpf)
+    {
+        $cpf = str_replace('.', '', $cpf);
+        $cpf = str_replace('-', '', $cpf);
+        $this->attributes['cpf'] = $cpf;
+    }
+
+    public function getCpfAttribute($cpf)
+    {
+        return substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 7, 3) . '-' . substr($cpf, -2);
+    }
+
+    public function setTelefoneAttribute($telefone)
+    {
+        $telefone = str_replace('(', '', $telefone);
+        $telefone = str_replace(')', '', $telefone);
+        $telefone = str_replace('-', '', $telefone);
+        $telefone = str_replace(' ', '', $telefone);
+        $this->attributes['telefone'] = $telefone;
+    }
+
+    public function getTelefoneAttribute($telefone)
+    {
+        if (strlen($telefone) == 11) {
+            return '(' . substr($telefone, 0, 2) . ') ' . substr($telefone, 2, 5) . '-' . substr($telefone, 7, 4);
+        } else {
+            return '(' . substr($telefone, 0, 2) . ') ' . substr($telefone, 2, 4) . '-' . substr($telefone, 6, 4);
+        }
+    }
+}
