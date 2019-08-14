8fd6743fc129f0df568c96393362c3ce4833db36
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2019-08-13 21:30:53 -0300

    Adicionando rota home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Projeto consolide
+            </div>
+
+            <div class="links">
+                <a href="{{ url('/marcas') }}">Marcas</a>
+            </div>
+        </div>
+    </div>
+@endsection
